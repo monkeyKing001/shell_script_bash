0aa233b822d0e3ac1de9f97398f9aad9eabd21d7
monkeyKing001 <2018121152@yonsei.ac.kr>
2021-07-13 12:24:59 +0900

    13.07

--- myread.sh
+++ myread.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+count=0
 
+exec   ## exec makes script be ignored and 
+exec 2> /dev/null ## exec can redirect 0=stdinput 1=stdoutput 2=stderror
 for file in $@
 do
 	if [ -f ${file} ]; then
-	wc -l ${file}
-		elif [ -d ${filse} ];then
-			for subtitle in ${file}/*
-			do
-				wc -l ${subtitle}
-			done
+	wc -l ${file} 
+	let count=count+1
+	elif [ -d ${file} ];then
+		for subtitle in ${file}/*
+		do
+			wc -l ${subtitle} 
+	done
 	fi
-done
+done >> result2.txt
 
+
+
+echo "Total count is ${count}"
 
